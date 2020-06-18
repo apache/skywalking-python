b02268936a0c71bff45da6ef5e81da87ec146191
Lei Sun <leix.sun@qq.com>
2020-06-18 15:11:57 +0800

    When werkzeug wsgi handler is detected, wrap run_wsgi to add skywalking instrument code.

--- skywalking/plugins/sw_http_server/__init__.py
+++ skywalking/plugins/sw_http_server/__init__.py
@@ -34,32 +34,63 @@ def install():
 
         _handle = BaseHTTPRequestHandler.handle
 
-        def _sw_handle(this: BaseHTTPRequestHandler):
-            http_methods = ('GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH')
-            for method in http_methods:
-                _wrap_do_method(this, method)
-            _handle(this)
-
-        def _wrap_do_method(this, method):
-            if hasattr(this, 'do_' + method) and inspect.ismethod(getattr(this, 'do_' + method)):
-                _do_method = getattr(this, 'do_' + method)
-
-                def _sw_do_method():
-                    context = get_context()
-                    carrier = Carrier()
-                    for item in carrier:
-                        item.val = this.headers[item.key.capitalize()]
-                    with context.new_entry_span(op=this.path, carrier=carrier) as span:
-                        span.layer = Layer.Http
-                        span.component = Component.General
-                        span.peer = '%s:%s' % this.client_address
-                        span.tag(Tag(key=tags.HttpMethod, val=method))
-
-                        _do_method()
-
-                setattr(this, 'do_' + method, _sw_do_method)
+        def _sw_handle(handler: BaseHTTPRequestHandler):
+            clazz = handler.__class__
+            if 'werkzeug.serving.WSGIRequestHandler' == ".".join([clazz.__module__, clazz.__name__]):
+                wrap_werkzeug_request_handler(handler)
+            else:
+                wrap_default_request_handler(handler)
+            _handle(handler)
 
         BaseHTTPRequestHandler.handle = _sw_handle
+
     except Exception:
         logger.warning('failed to install plugin %s', __name__)
         traceback.print_exc()
+
+
+def wrap_werkzeug_request_handler(handler):
+    """
+    Wrap run_wsgi of werkzeug.serving.WSGIRequestHandler to add skywalking instrument code.
+    """
+    _run_wsgi = handler.run_wsgi
+
+    def _wrap_run_wsgi():
+        context = get_context()
+        carrier = Carrier()
+        for item in carrier:
+            item.val = handler.headers[item.key.capitalize()]
+        with context.new_entry_span(op=handler.path, carrier=carrier) as span:
+            span.layer = Layer.Http
+            span.component = Component.General
+            span.peer = '%s:%s' % handler.client_address
+            span.tag(Tag(key=tags.HttpMethod, val=handler.command))
+            return _run_wsgi()
+
+    handler.run_wsgi = _wrap_run_wsgi
+
+
+def wrap_default_request_handler(handler):
+    http_methods = ('GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH')
+    for method in http_methods:
+        _wrap_do_method(handler, method)
+
+
+def _wrap_do_method(handler, method):
+    if hasattr(handler, 'do_' + method) and inspect.ismethod(getattr(handler, 'do_' + method)):
+        _do_method = getattr(handler, 'do_' + method)
+
+        def _sw_do_method():
+            context = get_context()
+            carrier = Carrier()
+            for item in carrier:
+                item.val = handler.headers[item.key.capitalize()]
+            with context.new_entry_span(op=handler.path, carrier=carrier) as span:
+                span.layer = Layer.Http
+                span.component = Component.General
+                span.peer = '%s:%s' % handler.client_address
+                span.tag(Tag(key=tags.HttpMethod, val=method))
+
+                _do_method()
+
+        setattr(handler, 'do_' + method, _sw_do_method)
