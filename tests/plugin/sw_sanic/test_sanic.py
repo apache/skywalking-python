b77f551ac480198aa4e689a5705cd025173bfd09
kezhenxu94 <kezhenxu94@apache.org>
2020-12-16 23:18:34 +0800

    test: remove some of the versions to shorten test time

--- tests/plugin/sw_sanic/test_sanic.py
+++ tests/plugin/sw_sanic/test_sanic.py
@@ -32,9 +32,6 @@ class TestPlugin(TestPluginBase):
     @pytest.mark.parametrize('version', [
         'sanic==20.3.0',
         'sanic==20.6.0',
-        'sanic==20.6.1',
-        'sanic==20.6.2',
-        'sanic==20.6.3',
         'sanic==20.9.0',
         'sanic==20.9.1',
     ])
