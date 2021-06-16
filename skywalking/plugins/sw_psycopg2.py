6002ccc5e7463be8960304c30ecae55753f001dc
Tomasz Pytel <tompytel@gmail.com>
2021-06-16 20:51:25 -0300

    add component id for python Psycopg -> PostgreSQL (#124)

--- skywalking/plugins/sw_psycopg2.py
+++ skywalking/plugins/sw_psycopg2.py
@@ -40,7 +40,7 @@ def install():
 
             with get_context().new_exit_span(op="PostgreSLQ/Psycopg/execute", peer=peer) as span:
                 span.layer = Layer.Database
-                span.component = Component.PostgreSQL
+                span.component = Component.Psycopg
 
                 span.tag(Tag(key=tags.DbType, val="PostgreSQL"))
                 span.tag(Tag(key=tags.DbInstance, val=dsn['dbname']))
@@ -62,7 +62,7 @@ def install():
 
             with get_context().new_exit_span(op="PostgreSLQ/Psycopg/executemany", peer=peer) as span:
                 span.layer = Layer.Database
-                span.component = Component.PostgreSQL
+                span.component = Component.Psycopg
 
                 span.tag(Tag(key=tags.DbType, val="PostgreSQL"))
                 span.tag(Tag(key=tags.DbInstance, val=dsn['dbname']))
@@ -94,7 +94,7 @@ def install():
 
             with get_context().new_exit_span(op="PostgreSLQ/Psycopg/callproc", peer=peer) as span:
                 span.layer = Layer.Database
-                span.component = Component.PostgreSQL
+                span.component = Component.Psycopg
                 args = '(' + ('' if not parameters else ','.join(parameters)) + ')'
 
                 span.tag(Tag(key=tags.DbType, val="PostgreSQL"))
