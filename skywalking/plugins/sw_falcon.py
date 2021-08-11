82033540c048b2f1b0282dd034a97bb283e5c47d
Tomasz Pytel <tompytel@gmail.com>
2021-08-11 13:24:53 -0300

    fixes and tweaks

--- skywalking/plugins/sw_falcon.py
+++ skywalking/plugins/sw_falcon.py
@@ -15,84 +15,65 @@
 # limitations under the License.
 #
 
-from skywalking import Layer, Component
+from skywalking import Layer, Component, config
 from skywalking.trace.carrier import Carrier
-from skywalking.trace.context import get_context
+from skywalking.trace.context import get_context, NoopContext
 from skywalking.trace.span import NoopSpan
-from skywalking.trace.tags import TagHttpMethod, TagHttpURL, TagHttpParams, TagHttpStatusCode
+from skywalking.trace.tags import TagHttpMethod, TagHttpURL, TagHttpParams, TagHttpStatusCode, TagHttpStatusMsg
 
 
 def install():
-    from falcon import API, request, response
+    from falcon import API, request, RequestOptions
 
     _original_falcon_api = API.__call__
-    _original_falcon_handle_exception = API._handle_exception
-
-    def params_tostring(params):
-        return "\n".join([k + "=" + v for k, v in params.items()])
 
     def _sw_falcon_api(this: API, env, start_response):
         context = get_context()
         carrier = Carrier()
-        headers = get_headers(env)
+        req = request.Request(env, RequestOptions())
+        headers = req.headers
+        method = req.method
+
         for item in carrier:
-            key = item.key.replace("_", "-") if "_" in item.key else item.key
-            if key.capitalize() in headers:
-                item.val = headers[key.capitalize()]
-        with context.new_entry_span(op="/", carrier=carrier) as span:
+            key = item.key.upper()
+            if key in headers:
+                item.val = headers[key]
+
+        span = NoopSpan(NoopContext()) if config.ignore_http_method_check(method) \
+            else context.new_entry_span(op=req.path, carrier=carrier)
+
+        with span:
             span.layer = Layer.Http
             span.component = Component.Falcon
+            span.peer = req.remote_addr
 
-            from falcon import RequestOptions
-
-            req = request.Request(env, RequestOptions())
-            span.op = str(req.url).split("?")[0]
-            span.peer = "%s:%s" % (req.remote_addr, req.port)
-
-            span.tag(TagHttpMethod(req.method))
+            span.tag(TagHttpMethod(method))
             span.tag(TagHttpURL(str(req.url)))
+
             if req.params:
-                span.tag(TagHttpParams(params_tostring(req.params)[0:]))
+                span.tag(TagHttpParams(','.join([k + '=' + v for k, v in req.params.items()])))
 
-            resp = _original_falcon_api(this, env, start_response)
+            def _start_response(resp_status, headers):
+                try:
+                    code, msg = resp_status.split(' ', 1)
+                    code = int(code)
+                except Exception:
+                    code, msg = 500, 'Internal Server Error'
 
-            from falcon import ResponseOptions
+                if code >= 400:
+                    span.error_occurred = True
 
-            resp_obj = response.Response(ResponseOptions())
+                span.tag(TagHttpStatusCode(code))
+                span.tag(TagHttpStatusMsg(msg))
 
-            resp_status = parse_status(resp_obj.status)
-            if int(resp_status[0]) >= 400:
-                span.error_occurred = True
+                return start_response(resp_status, headers)
 
-            span.tag(TagHttpStatusCode(int(resp_status[0])))
+            try:
+                return _original_falcon_api(this, env, _start_response)
 
-            return resp
+            except Exception:
+                span.raised()
 
-    def _sw_handle_exception(this: API, req, resp, ex, params):
-        if ex is not None:
-            entry_span = get_context().active_span()
-            if entry_span is not None and type(entry_span) is not NoopSpan:
-                entry_span.raised()
-
-        return _original_falcon_handle_exception(this, req, resp, ex, params)
+                raise
 
     API.__call__ = _sw_falcon_api
-    API._handle_exception = _sw_handle_exception
-
-
-def get_headers(env):
-    headers = {}
-    wsgi_content_headers = frozenset(["CONTENT_TYPE", "CONTENT_LENGTH"])
-
-    for name, value in env.items():
-        if name.startswith("HTTP_"):
-            headers[name[5:].replace("_", "-")] = value
-
-        elif name in wsgi_content_headers:
-            headers[name.replace("_", "-")] = value
-
-    return headers
-
-
-def parse_status(status_str):
-    return status_str.split(" ") if status_str else [404, "status is empty"]
