4ef2b67b141b86279dbe159e0c3bcb12ab31d097
Tomasz Pytel <tompytel@gmail.com>
2021-10-21 10:22:33 -0300

    Add support for psycopg 3 (#168)

--- skywalking/plugins/sw_psycopg.py
+++ skywalking/plugins/sw_psycopg.py
@@ -28,9 +28,11 @@ support_matrix = {
 note = """"""
 
 
-def install():
-    import wrapt  # psycopg2 is read-only C extension objects so they need to be proxied
+def install_sync():
+    import wrapt  # psycopg is read-only C extension objects so they need to be proxied
     import psycopg
+
+    # synchronous
 
     class ProxyCursor(wrapt.ObjectProxy):
         def __init__(self, cur):
@@ -41,7 +43,7 @@ def install():
         def __enter__(self):
             return ProxyCursor(wrapt.ObjectProxy.__enter__(self))
 
-        def execute(self, query, vars=None):
+        def execute(self, query, vars=None, *args, **kwargs):
             dsn = self.connection.info.get_parameters()
             port = self.connection.info.port
             peer = dsn['host'] + ':' + str(port)
@@ -62,9 +64,9 @@ def install():
 
                     span.tag(TagDbSqlParameters('[' + text + ']'))
 
-                return self._self_cur.execute(query, vars)
+                return self._self_cur.execute(query, vars, *args, **kwargs)
 
-        def executemany(self, query, vars_list):
+        def executemany(self, query, vars_list, *args, **kwargs):
             dsn = self.connection.info.get_parameters()
             port = self.connection.info.port
             peer = dsn['host'] + ':' + str(port)
@@ -95,23 +97,30 @@ def install():
 
                     span.tag(TagDbSqlParameters('[' + ','.join(text_list) + ']'))
 
-                return self._self_cur.executemany(query, vars_list)
+                return self._self_cur.executemany(query, vars_list, *args, **kwargs)
 
-        def callproc(self, procname, parameters=None):
+        def stream(self, query, vars=None, *args, **kwargs):
             dsn = self.connection.info.get_parameters()
             port = self.connection.info.port
             peer = dsn['host'] + ':' + str(port)
 
-            with get_context().new_exit_span(op="PostgreSLQ/Psycopg/callproc", peer=peer,
+            with get_context().new_exit_span(op="PostgreSLQ/Psycopg/stream", peer=peer,
                                              component=Component.Psycopg) as span:
                 span.layer = Layer.Database
-                args = '(' + ('' if not parameters else ','.join(parameters)) + ')'
 
                 span.tag(TagDbType("PostgreSQL"))
                 span.tag(TagDbInstance(dsn['dbname']))
-                span.tag(TagDbStatement(procname + args))
+                span.tag(TagDbStatement(query))
 
-                return self._self_cur.callproc(procname, parameters)
+                if config.sql_parameters_length and vars is not None:
+                    text = ','.join(str(v) for v in vars)
+
+                    if len(text) > config.sql_parameters_length:
+                        text = text[:config.sql_parameters_length] + '...'
+
+                    span.tag(TagDbSqlParameters('[' + text + ']'))
+
+                yield from self._self_cur.stream(query, vars, *args, **kwargs)
 
     class ProxyConnection(wrapt.ObjectProxy):
         def __init__(self, conn):
@@ -128,5 +137,129 @@ def install():
     def connect(*args, **kwargs):
         return ProxyConnection(_connect(*args, **kwargs))
 
-    _connect = psycopg.connect
+    _connect = psycopg.Connection.connect
+    psycopg.Connection.connect = connect
     psycopg.connect = connect
+
+
+def install_async():
+    import wrapt  # psycopg is read-only C extension objects so they need to be proxied
+    import psycopg
+
+    class ProxyAsyncCursor(wrapt.ObjectProxy):
+        def __init__(self, cur):
+            wrapt.ObjectProxy.__init__(self, cur)
+
+            self._self_cur = cur
+
+        async def __aenter__(self):
+            return self  # return ProxyAsyncCursor(await self._self_cur.__aenter__())  # may be necessary in future version of psycopg if __aenter__() does not return self
+
+        async def __aexit__(self, exc_type, exc_val, exc_tb):
+            return await self._self_cur.__aexit__(exc_type, exc_val, exc_tb)
+
+        async def execute(self, query, vars=None, *args, **kwargs):
+            dsn = self.connection.info.get_parameters()
+            port = self.connection.info.port
+            peer = dsn['host'] + ':' + str(port)
+
+            with get_context().new_exit_span(op="PostgreSLQ/Psycopg/execute", peer=peer,
+                                             component=Component.Psycopg) as span:
+                span.layer = Layer.Database
+
+                span.tag(TagDbType("PostgreSQL"))
+                span.tag(TagDbInstance(dsn['dbname']))
+                span.tag(TagDbStatement(query))
+
+                if config.sql_parameters_length and vars is not None:
+                    text = ','.join(str(v) for v in vars)
+
+                    if len(text) > config.sql_parameters_length:
+                        text = text[:config.sql_parameters_length] + '...'
+
+                    span.tag(TagDbSqlParameters('[' + text + ']'))
+
+                return await self._self_cur.execute(query, vars, *args, **kwargs)
+
+        async def executemany(self, query, vars_list, *args, **kwargs):
+            dsn = self.connection.info.get_parameters()
+            port = self.connection.info.port
+            peer = dsn['host'] + ':' + str(port)
+
+            with get_context().new_exit_span(op="PostgreSLQ/Psycopg/executemany", peer=peer,
+                                             component=Component.Psycopg) as span:
+                span.layer = Layer.Database
+
+                span.tag(TagDbType("PostgreSQL"))
+                span.tag(TagDbInstance(dsn['dbname']))
+                span.tag(TagDbStatement(query))
+
+                if config.sql_parameters_length:
+                    max_len = config.sql_parameters_length
+                    total_len = 0
+                    text_list = []
+
+                    for vars in vars_list:
+                        text = '[' + ','.join(str(v) for v in vars) + ']'
+                        total_len += len(text)
+
+                        if total_len > max_len:
+                            text_list.append(text[:max_len - total_len] + '...')
+
+                            break
+
+                        text_list.append(text)
+
+                    span.tag(TagDbSqlParameters('[' + ','.join(text_list) + ']'))
+
+                return await self._self_cur.executemany(query, vars_list, *args, **kwargs)
+
+        async def stream(self, query, vars=None, *args, **kwargs):
+            dsn = self.connection.info.get_parameters()
+            port = self.connection.info.port
+            peer = dsn['host'] + ':' + str(port)
+
+            with get_context().new_exit_span(op="PostgreSLQ/Psycopg/stream", peer=peer,
+                                             component=Component.Psycopg) as span:
+                span.layer = Layer.Database
+
+                span.tag(TagDbType("PostgreSQL"))
+                span.tag(TagDbInstance(dsn['dbname']))
+                span.tag(TagDbStatement(query))
+
+                if config.sql_parameters_length and vars is not None:
+                    text = ','.join(str(v) for v in vars)
+
+                    if len(text) > config.sql_parameters_length:
+                        text = text[:config.sql_parameters_length] + '...'
+
+                    span.tag(TagDbSqlParameters('[' + text + ']'))
+
+                async for r in self._self_cur.stream(query, vars, *args, **kwargs):
+                    yield r
+
+    class ProxyAsyncConnection(wrapt.ObjectProxy):
+        def __init__(self, conn):
+            wrapt.ObjectProxy.__init__(self, conn)
+
+            self._self_conn = conn
+
+        async def __aenter__(self):
+            return self  # return ProxyAsyncConnection(await self._self_conn.__aenter__())  # may be necessary in future version of psycopg if __aenter__() does not return self
+
+        async def __aexit__(self, exc_type, exc_val, exc_tb):
+            return await self._self_conn.__aexit__(exc_type, exc_val, exc_tb)
+
+        def cursor(self, *args, **kwargs):
+            return ProxyAsyncCursor(self._self_conn.cursor(*args, **kwargs))
+
+    async def aconnect(*args, **kwargs):
+        return ProxyAsyncConnection(await _aconnect(*args, **kwargs))
+
+    _aconnect = psycopg.AsyncConnection.connect
+    psycopg.AsyncConnection.connect = aconnect
+
+
+def install():
+    install_sync()
+    install_async()
