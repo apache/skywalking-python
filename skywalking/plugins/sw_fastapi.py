1ca9ae3cf7f1dd069d2031210c3022f28cf4d1e7
wanglei1078 <wanglei1078@jd.com>
2022-02-10 11:30:47 +0800

    add fastapi

--- skywalking/plugins/sw_fastapi.py
+++ skywalking/plugins/sw_fastapi.py
@@ -14,54 +14,68 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from asyncio import coroutine
-from inspect import iscoroutinefunction, isawaitable
-
-from starlette.types import ASGIApp, Receive, Scope, Send, Message
+from starlette.exceptions import HTTPException
+from starlette.requests import Request
+from starlette.types import Receive, Scope, Send, Message
 from skywalking import Layer, Component, config
 from skywalking.trace.carrier import Carrier
 from skywalking.trace.context import get_context, NoopContext
 from skywalking.trace.span import NoopSpan
 from skywalking.trace.tags import TagHttpMethod, TagHttpURL, TagHttpStatusCode, TagHttpParams
 
-link_vector = ['https://flask.palletsprojects.com']
+link_vector = ['https://fastapi.tiangolo.com']
 support_matrix = {
     'fastapi': {
-        '>=3.6': ['1.1', '2.0']  # 1.1 to be removed in near future
+        '>=3.6': ['1.0']
     }
 }
 note = """"""
 
 
 def install():
-    from fastapi import routing
-    from fastapi.routing import APIRoute
+    from starlette.exceptions import ExceptionMiddleware
 
-    from starlette.responses import Response
+    _original_fast_api = ExceptionMiddleware.__call__
 
-    old_execute = APIRoute.handle
-    APIRoute.handle = _gen_sw_get_response_func(old_execute)
+    def params_tostring(params):
+        return '\n'.join([f"{k}=[{','.join(params.getlist(k))}]" for k, _ in params.items()])
 
-def _gen_sw_get_response_func(old_execute):
-    from fastapi import routing
-    print(123)
-    is_coroutine = routing.iscoroutinefunction_or_partial(old_execute)
-    if is_coroutine:
-        async def _sw_get_response(self, scope: Scope, receive: Receive, send: Send):
-            from starlette.requests import Request
-            req = Request(scope, receive=receive, send=send)
-            result = old_execute(self, scope, receive, send)
-            print(result)
-            if isawaitable(result):
-                result = await result
-                print(result)
-            return result
-    else:
-        @coroutine
-        def _sw_get_response(self, scope: Scope, receive: Receive, send: Send):
-            result = yield from old_execute(self, scope, receive, send)
-            print(result)
-            return result
-    return _sw_get_response
+    async def _sw_fast_api(self, scope: Scope, receive: Receive, send: Send):
+        from starlette.requests import Request
 
+        req = Request(scope, receive=receive, send=send)
+        carrier = Carrier()
+        method = req.method
 
+        for item in carrier:
+            if item.key.capitalize() in req.headers:
+                item.val = req.headers[item.key.capitalize()]
+
+        span = NoopSpan(NoopContext()) if config.ignore_http_method_check(method) \
+            else get_context().new_entry_span(op=dict(scope)["path"], carrier=carrier, inherit=Component.General)
+
+        with span:
+            span.layer = Layer.Http
+            span.component = Component.FastApi
+            span.peer = f"{req.client.host}:{req.client.port}"
+            span.tag(TagHttpMethod(method))
+            span.tag(TagHttpURL(req.url._url.split('?')[0]))
+            if config.fastapi_collect_http_params and req.values:
+                span.tag(TagHttpParams(params_tostring(req.values)[0:config.http_params_length_threshold]))
+
+            status_code = 500
+
+            async def wrapped_send(message: Message) -> None:
+                if message['type'] == 'http.response.start':
+                    nonlocal status_code
+                    status_code = message['status']
+                await send(message)
+
+            print(status_code)
+            try:
+                resp = await _original_fast_api(self, scope, receive, wrapped_send)
+            finally:
+                span.tag(TagHttpStatusCode(status_code))
+        return resp
+
+    ExceptionMiddleware.__call__ = _sw_fast_api
